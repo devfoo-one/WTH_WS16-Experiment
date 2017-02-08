d3bb67828d49d948cfde5a4a07521f3e9aa53a3f
Tom Oberhauser <tom@devfoo.de>
2017-02-08 19:54:57 +0100

    final dataset

--- data/generateCSV.py
+++ data/generateCSV.py
@@ -5,6 +5,11 @@ Data files must be stored in directories named like DDD-dd (i.e. jan-20)
 
 import os, re, csv, time, string
 
+def getAsciiIdString(n):
+    first =  string.ascii_lowercase[int(n / 26)]
+    second = string.ascii_lowercase[n % 26]
+    return first+second
+
 # __time_str = str(int(time.time()))
 # data_filename = 'data' + __time_str + '.csv'
 # data_plot_filename = 'data_plot' + __time_str + '.csv'
@@ -38,8 +43,14 @@ with open(data_filename, 'w') as data_file:
 
 with open(data_plot_filename, 'w') as data_plot_file:
     data_plot_writer = csv.writer(data_plot_file)
-    id_characters =list(string.ascii_lowercase[0:len(data[100])])  # get id characters
+    id_characters = []
+    for i in range(0, len(data[100])):
+        print(i, getAsciiIdString(i))
+        id_characters.append(getAsciiIdString(i))
+
+    # id_characters = getAsciiIdString()list(string.ascii_lowercase[0:len(data[100])])  # get id characters
     data_plot_writer.writerow(('frequenz',) + tuple(id_characters))
     for f in frequencies:
         data_plot_writer.writerow((f,) + tuple(data[f]))
         print(f, data[f])
+
